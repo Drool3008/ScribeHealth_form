b0ff9e02beaec87d08943eb846da5932ad5eb374
Keshav Dubey <keshavdubey.design@gmail.com>
2026-03-21 12:36:36 +0530

    fix: make basePath conditional for local development servers

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   output: 'export',
-  basePath: '/ScribeHealth_form',
+  basePath: isProd ? '/ScribeHealth_form' : '',
   images: {
     unoptimized: true,
   },
