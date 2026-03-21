0c0ed6a0fabb8b51fbd96b1fc1df27392e25d76e
Keshav Dubey <keshavdubey.design@gmail.com>
2026-03-21 16:48:02 +0530

    Fix: Added trailingSlash for GitHub Pages routing alignment flawlessly

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
   output: 'export',
+  trailingSlash: true,
   basePath: isProd ? '/ScribeHealth_form' : '',
   images: {
     unoptimized: true,
